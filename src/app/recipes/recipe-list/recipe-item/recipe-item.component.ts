6864e284171f6bbea9f95fd6c020423a33b79ec7
amyas <amyas.gilbert@room58.com>
2023-11-17 14:10:56 +0000

    finished section 3 - basics

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
+
 export class RecipeItemComponent {
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from "../recipe.model";
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('testipe', 'testription', 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg'),
+    new Recipe('testipe2', 'testription', 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg'),
+    new Recipe('testipe3', 'testription', 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg'),
+    new Recipe('testipe4', 'testription', 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Test.svg'),
+  ]
 
+  constructor() {
+  }
 }
